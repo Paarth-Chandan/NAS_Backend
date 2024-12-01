0be3f607467bc3ac33fa1b1a5c5c14396537f848
paarth3 <2205822@kiit.ac.in>
2024-12-02 00:35:10 +0530

    v2

--- src/main/java/org/workspace/nas_backend/NasBackendApplication.java
+++ src/main/java/org/workspace/nas_backend/NasBackendApplication.java
@@ -19,7 +19,7 @@ public class NasBackendApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:3000")
+						.allowedOrigins("*")
 						.allowedMethods("GET", "POST", "PUT", "DELETE");
 			}
 		};
