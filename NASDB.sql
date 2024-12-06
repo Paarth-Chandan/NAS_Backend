4842ed751d69e52ad4722c173e1f36a60e72b260
paarth3 <2205822@kiit.ac.in>
2024-12-06 13:42:28 +0530

    403 fixing

--- NASDB.sql
+++ NASDB.sql
@@ -3,20 +3,20 @@ USE NASDB;
 
 DROP TABLE IF EXISTS USERS;
 CREATE TABLE USERS (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       username VARCHAR(50) NOT NULL UNIQUE,
+                       email VARCHAR(100) NOT NULL UNIQUE,
+                       password VARCHAR(255) NOT NULL
 );
 
 DROP TABLE IF EXISTS responses;
 CREATE TABLE responses (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    user_input TEXT NOT NULL,
-    question1 TEXT,
-    question2 TEXT,
-    question3 TEXT,
-    question4 TEXT
+                           id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                           user_input TEXT NOT NULL,
+                           question1 TEXT,
+                           question2 TEXT,
+                           question3 TEXT,
+                           question4 TEXT
 );
 
 DROP TABLE IF EXISTS ARTICLE;
